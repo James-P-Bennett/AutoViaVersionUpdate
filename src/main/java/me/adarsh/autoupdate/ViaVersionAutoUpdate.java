00f1996e634e3d3832e74d080ef299d4c7757b96
James Bennett <85420204+James-P-Bennett@users.noreply.github.com>
2023-10-28 10:26:49 -0500

    Update ViaVersionAutoUpdate.java

--- src/main/java/me/adarsh/autoupdate/ViaVersionAutoUpdate.java
+++ src/main/java/me/adarsh/autoupdate/ViaVersionAutoUpdate.java
@@ -21,34 +21,4 @@ public class ViaVersionAutoUpdate {
     public IPlugin getPlugin() {
         return plugin;
     }
-
-    /**
-     * Restarts the server after a 5 minute countdown
-     */
-    public void startRestartCountdown() {
-        // 0 seconds from now
-        plugin.runTaskLaterAsync(() -> {
-            plugin.broadcastMessage("[ViaVersionAutoUpdate] Server restart in 5 minutes");
-        }, 0);
-
-        // 3 minutes from now
-        plugin.runTaskLaterAsync(() -> {
-            plugin.broadcastMessage("[ViaVersionAutoUpdate] Server restart in 2 minutes");
-        }, 3*60);
-
-        // 4 minutes from now
-        plugin.runTaskLaterAsync(() -> {
-            plugin.broadcastMessage("[ViaVersionAutoUpdate] Server restart in 1 minute");
-        }, 4*60);
-
-        // 4 minutes and a half from now
-        plugin.runTaskLaterAsync(() -> {
-            plugin.broadcastMessage("[ViaVersionAutoUpdate] Server restart in 30 seconds");
-        }, 4*60 + 30);
-
-        // 5 minutes from now
-        plugin.runTaskLaterAsync(() -> {
-            plugin.restart();
-        }, 5*60);
-    }
 }
